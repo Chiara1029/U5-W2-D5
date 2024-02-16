497d17d81ce2b0820cd17231d0c4c6dcb60172d1
Chiara Puleio <chiarapuleio@yahoo.it>
2024-02-16 16:48:39 +0100

    null error (prev. commit) fixed

--- src/main/java/com/chiarapuleio/devicesmanagement/payloads/EmployeeUuidDTO.java
+++ src/main/java/com/chiarapuleio/devicesmanagement/payloads/EmployeeUuidDTO.java
@@ -1,12 +1,13 @@
 package com.chiarapuleio.devicesmanagement.payloads;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record EmployeeUuidDTO(
-        @NotNull(message = "ID required.")
+        @NotEmpty(message = "ID required.")
         @Size(min = 3, max = 36, message = "ID max value is 36 characters.")
         UUID id
 ) {
